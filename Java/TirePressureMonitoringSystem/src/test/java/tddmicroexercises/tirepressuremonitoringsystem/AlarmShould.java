387d79e93f1b7cefdb29b04a371a3867931b79a2
Sherlock Qiao <sherlockq@gmail.com>
2019-08-05 16:53:18 +0100

    alarm

--- Java/TirePressureMonitoringSystem/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/AlarmShould.java
+++ Java/TirePressureMonitoringSystem/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/AlarmShould.java
@@ -8,6 +8,7 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
 import static org.junit.Assert.*;
+import static org.mockito.BDDMockito.given;
 
 @RunWith(MockitoJUnitRunner.class)
 public class AlarmShould {
@@ -21,4 +22,30 @@ public class AlarmShould {
     public void stay_off_by_default() {
         assertEquals(false, alarm.isAlarmOn());
     }
+
+    @Test
+    public void stay_off_when_sensor_within_range() {
+        given(sensor.popNextPressurePsiValue()).willReturn(20d);
+        alarm.check();
+        assertFalse(alarm.isAlarmOn());
+    }
+
+    @Test
+    public void name() {
+    }
+
+    //    @Test
+//    public void should_send_greeting_email_to_employee() {
+//        System.setOut(new PrintStream(consoleContent));
+//        given(clock.monthDay()).willReturn(TODAY);
+//        Employee employee = anEmployee().build();
+//        given(employeeRepository.findEmployeesBornOn(MonthDay.of(CURRENT_MONTH, CURRENT_DAY_OF_MONTH))).willReturn(Collections.singletonList(employee));
+//
+//        birthdayGreeter.sendGreetings();
+//
+//        String actual = consoleContent.toString();
+//        assertThat(actual)
+//                .isEqualTo("To:" + employee.getEmail() + ", Subject: Happy birthday!, Message: Happy birthday, dear " + employee.getFirstName()+"!");
+//
+//    }
 }
